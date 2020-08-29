a84fdb6c98a804d56fcbeff67423875b1d5440b6
lathisha <lathisha.n@gmail.com>
2020-08-29 14:30:04 -0700

    changed models to match design front end

--- models/vehicle.js
+++ models/vehicle.js
@@ -4,22 +4,14 @@ const Schema = mongoose.Schema;
 // the vehicle model is whatever they are CREATING or inputting
 const vehicleSchema = new Schema({
   nickname: { type: String },
-  // owner references email and password
-  owner: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "User"
-  },
-  firstName: { type: String },
-  lastName: { type: String },
   make: { type: String, required: true },
   model: { type: String, required: true },
   year: { type: Number, required: true },
-  maintenance: {
+  VIN: { type: Number, required: true },
+  maintenance: [{
     type: Schema.Types.ObjectId,
-    required: true,
     ref: "Maintenance"
-  }
+  }]
 });
 
 const Vehicle = mongoose.model("Vehicle", vehicleSchema);
