1f05e636994d8971b6625f29d70fc3ebb01c2a4a
Gedalya Krycer <gedalya@krycer.com>
2020-09-01 13:51:49 -0700

    Added intial LoginNavBar Component

--- client/src/components/Login/LoginNavBar/index.js
+++ client/src/components/Login/LoginNavBar/index.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import logo from "../../images/driverkicklogo.png";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Logo from "../../../images/global/drive-kick-logo-tan.svg";
 
 function LoginNavBar() {
-    return (
-        <div>
-            <Navbar className="container navbar justify-content-between flex-column flex-sm-row">
-                <Navbar.Brand href="/">
-                    <img
-                        src={logo}
-                        className="d-inline-block align-top"
-                        alt="Drive Kick Logo"
-                    />
-                </Navbar.Brand>
+    const createAccount = () => {
+        // Should direct people to the sign up page
+        console.log("createAccount button")
+    }
 
-                <Nav>
-                    <Button className="btnGroup">Create Account</Button>
-                </Nav>
-            </Navbar>
-        </div>
+    return (
+
+        <Navbar className="container navbar justify-content-between flex-column flex-sm-row">
+            <Navbar.Brand href="/">
+                <img
+                    src={Logo}
+                    className="d-inline-block align-top"
+                    alt="Drive Kick Logo"
+                />
+            </Navbar.Brand>
+            <Nav>
+
+                <button
+                    type="button"
+                    className="btn"
+                    onClick={createAccount}
+                >
+                    Create Account
+                        </button>
+
+            </Nav>
+        </Navbar>
     );
 }
 
