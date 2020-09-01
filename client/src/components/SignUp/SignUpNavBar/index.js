3d5e51bc611b9c338034b8634ef24bdf695d308e
Gedalya Krycer <gedalya@krycer.com>
2020-09-01 16:31:10 -0700

    Added initial component for SignUpNav

--- client/src/components/SignUp/SignUpNavBar/index.js
+++ client/src/components/SignUp/SignUpNavBar/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Logo from "../../../images/global/drive-kick-logo-tan.svg";
 
 function SignUpNavBar() {
+    const login = () => {
+        // Should direct people to the login page
+        console.log("login button")
+    }
     return (
-        <div>
+        <Navbar className="container navbar justify-content-between flex-column flex-sm-row">
+            <Navbar.Brand href="/">
+                <img
+                    src={Logo}
+                    className="d-inline-block align-top"
+                    alt="Drive Kick Logo"
+                />
+            </Navbar.Brand>
+            <Nav>
 
-        </div>
+                <button
+                    type="button"
+                    className="btn"
+                    onClick={login}
+                >
+                    Login
+                        </button>
+
+            </Nav>
+        </Navbar>
     );
 }
 
