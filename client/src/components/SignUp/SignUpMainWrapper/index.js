d6521c64ddc73cc92b647ef06e9339e3a2607f7d
Gedalya Krycer <gedalya@krycer.com>
2020-09-01 16:39:24 -0700

    Added initial component for SignUpWrapper

--- client/src/components/SignUp/SignUpMainWrapper/index.js
+++ client/src/components/SignUp/SignUpMainWrapper/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import "./style.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-function SignUpMainWrapper() {
+function SignUpMainWrapper({ children }) {
     return (
-        <div>
-
-        </div>
+        <section className="sign-wrap__bg">
+            <div className="container">
+                <Row>
+                    <Col lg={5}>
+                        {children}
+                    </Col>
+                    <Col lg={1}></Col>
+                    <Col lg={6}>
+                        <h1 className="text-white">
+                            Kick Your<br />
+                            <span className="g__fiery-orange--txt">Car Health</span><br />
+                            Into High<br />
+                        Gear
+                    </h1>
+                    </Col>
+                </Row>
+            </div>
+        </section>
     );
 }
 
