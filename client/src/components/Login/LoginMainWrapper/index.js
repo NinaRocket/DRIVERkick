ce4b81d5a6059b09c238724b284a364bcce70097
Gedalya Krycer <gedalya@krycer.com>
2020-09-01 14:16:43 -0700

    Added intial LoginMainWrapper Component

--- client/src/components/Login/LoginMainWrapper/index.js
+++ client/src/components/Login/LoginMainWrapper/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import "./style.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-function LoginMainWrapper() {
+function LoginMainWrapper({ children }) {
     return (
-        <div>
-
-        </div>
+        <section className="log-wrap__bg">
+            <Row>
+                <Col lg={5}>
+                    {children}
+                </Col>
+                <Col lg={1}></Col>
+                <Col lg={6}>
+                    <h1 className="text-white">
+                        Welcome<br />
+                        Back<br />
+                        Let<span className="g__fiery-orange--txt">’</span>s get<br />
+                        tracking
+                    </h1>
+                </Col>
+            </Row>
+        </section>
     );
 }
 
