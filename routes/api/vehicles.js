77b3d7aa58a917aae952e55bb62829ba6c67b600
Jake <j.t.cravey1991@gmail.com>
2020-08-31 19:58:53 -0700

    pulling

--- routes/api/vehicles.js
+++ routes/api/vehicles.js
@@ -3,11 +3,14 @@ const vehicleController = require("../../controllers/vehicleController");
 
 router.route("/").post(vehicleController.create);
 
-router
-  .route("/:id")
+router.route("/:id")
   // get vehicle by first finding owner
   .get(vehicleController.findByOwner)
   // update vehicle
   .put(vehicleController.update);
+
+router.route("/:id")
+  // gets vehicle oil change mileage
+  .get()
 
 module.exports = router;
