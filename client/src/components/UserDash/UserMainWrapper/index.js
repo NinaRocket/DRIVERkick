282b7452384c86b2848c6406e21274b640f35c0e
Gedalya Krycer <gedalya@krycer.com>
2020-09-02 04:40:02 -0700

    Added base component UserVehicleCard with editable text fields, but this has bugs still

--- client/src/components/UserDash/UserMainWrapper/index.js
+++ client/src/components/UserDash/UserMainWrapper/index.js
@@ -19,19 +19,7 @@ function UserMainWrapper({ children }) {
     console.log(user);
 
 
-    // Edit Code ——————————|
-    const [edit, setEdit] = useState(false);
-    const [customTxt, setCustomTxt] = useState();
-    const editFunc = () => {
-        setEdit(true)
-    }
 
-    const saveFunc = () => {
-        const ctext = document.getElementById("intro");
-        setCustomTxt(ctext)
-        setEdit(false)
-    }
-    // Edit Code ——————————|
 
 
     return (
@@ -42,11 +30,7 @@ function UserMainWrapper({ children }) {
                         <h1 className="g__dash-h1">Welcome {user.firstName}</h1>
                         <p>Here are the vehicles you are tracking maintenance for. Add new ones
                             at any time!</p>
-                        {/* // Edit Code ——————————|     */}
-                        <h1 contentEditable={edit} id="editVal">{!customTxt ? "Edit Me" : customTxt}</h1>
-                        <button onClick={editFunc}>Edit</button>
-                        <button onClick={saveFunc}>Save</button>
-                        {/* // Edit Code ——————————|     */}
+
                     </Col>
                     <Col lg={1}></Col>
                     <Col lg={8}>{children}</Col>
--- client/src/components/UserDash/UserVehicleCard/index.js
+++ client/src/components/UserDash/UserVehicleCard/index.js
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import "./style.css";
 
-function VehicleNavBar() {
+function VehicleNavBar({ trackMaintenanceBtn }) {
+    // Edit Code ——————————|
+    const [edit, setEdit] = useState(false);
+    const [customTxt, setCustomTxt] = useState();
+    
+    const editFunc = () => {
+        const styler = document.getElementById("editVal");
+        styler.setAttribute("style", "color:red; border: 1px solid blue;");
+        setEdit(true)
+    }
+
+    const saveFunc = () => {
+        const values = document.getElementById("editVal");
+        values.removeAttribute("style");
+        setCustomTxt(values)
+        setEdit(false)
+    }
+    // Edit Code ——————————|
     return (
         <div>
-
+            <button onClick={editFunc}>Edit</button>
+            <button onClick={saveFunc}>Save</button>
+            {/* Row */}
+            <div>
+                {/* Image Col */}
+                <div>Image</div>
+                {/* Content Col */}
+                <div>
+                    {/* Header Row & Bottom-border*/}
+                    <div>
+                        <div>
+                            <h4>Car Nickname</h4>
+                            {/* // Edit Code ——————————|     */}
+                            <h3 contentEditable={edit} id="editVal">{!customTxt ? "Edit Me" : customTxt}</h3>
+                            {/* // Edit Code ——————————|     */}
+                        </div>
+                        <button onClick={trackMaintenanceBtn}>Track Maintenance</button>
+                    </div>
+                    {/* Car Specs */}
+                    <div>
+                        {/* Row 1 */}
+                        <div>
+                            <div>
+                                <h4>Make</h4>
+                                <h3>Toyota</h3>
+                            </div>
+                            <div>
+                                <h4>Year</h4>
+                                <h3>2001</h3>
+                            </div>
+                        </div>
+                        {/* Row 2 */}
+                        <div>
+                            <div>
+                                <h4>Owner</h4>
+                                <h3>Linnea Gear</h3>
+                            </div>
+                            <div>
+                                <h4>Model</h4>
+                                <h3>Coup</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
