33132991982a2e671d0de4c90119ad64881e59b7
Gedalya Krycer <gedalya@krycer.com>
2020-09-01 19:02:22 -0700

    Added initial component for UserMainWrapper

--- client/src/components/UserDash/UserMainWrapper/index.js
+++ client/src/components/UserDash/UserMainWrapper/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import "./style.css";
+import { useParams } from "react-router-dom";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import API from "../../../utils/API";
 
-function UserMainWrapper() {
+function UserMainWrapper({ children }) {
+    const [user, setUser] = useState({});
+
+    const { id } = useParams();
+    useEffect(() => {
+        API.getUser(id)
+            .then((res) => setUser(res.data))
+            .catch((err) => console.log(err));
+        console.log(user);
+    }, []);
+    console.log(user);
+
     return (
-        <div>
-
-        </div>
+        <section className="g__dashboard-wrapper">
+            <div className="container">
+                <Row>
+                    <Col lg={3}>
+                        <h1 className="g__dash-h1">Welcome {user.firstName}</h1>
+                        <p>Here are the vehicles you are tracking maintenance for. Add new ones
+                            at any time!</p>
+                    </Col>
+                    <Col lg={1}></Col>
+                    <Col lg={8}>{children}</Col>
+                </Row>
+            </div>
+        </section>
     );
 }
 
