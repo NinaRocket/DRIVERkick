034fddbb62f26979ea21b88a5e39939d9da2eeaa
Gedalya Krycer <gedalya@krycer.com>
2020-09-05 06:48:47 -0700

    Added roof text block

--- client/src/components/Home/HomeFeatureBlock/index.js
+++ client/src/components/Home/HomeFeatureBlock/index.js
@@ -5,7 +5,9 @@ function HomeFeatureBlock() {
     return (
         <section className="g__deep-blue--bg">
             <div className="container home-feat__block" id="featBlock">
-
+                <div className="home-feat__roof-txt">
+                    <h4>The Ultimate</h4>
+                </div>
             </div>
         </section>
     );
