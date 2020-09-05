e5ed86ffe8f907630b535a64b5c672646a698dfc
NRocket <ninamrocket@gmail.com>
2020-09-05 08:46:04 -0700

    login page

--- client/src/components/Login/LoginPage/index.js
+++ client/src/components/Login/LoginPage/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import LoginNavBar from '../LoginNavBar';
-import LoginMainWrapper from '../LoginMainWrapper';
-import LoginForm from '../LoginForm';
+import React from "react";
+import LoginNavBar from "../LoginNavBar";
+import LoginMainWrapper from "../LoginMainWrapper";
+import LoginForm from "../LoginForm";
 
 function LoginPage() {
-    return (
-        <div>
-            <LoginNavBar />
-            <LoginMainWrapper>
-                <LoginForm />
-            </LoginMainWrapper>
-        </div>
-    );
+  return (
+    <div>
+      <LoginNavBar />
+      <LoginMainWrapper>
+        <LoginForm />
+      </LoginMainWrapper>
+    </div>
+  );
 }
 
 export default LoginPage;
