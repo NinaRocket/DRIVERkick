1fdf1beca3b9f9fc09e1c2c424bb379b2f1e3e3b
Gedalya Krycer <gedalya@krycer.com>
2020-09-07 11:22:19 -0700

    Added base modal

--- client/src/components/VehicleDash/MileageTrackerCard/index.js
+++ client/src/components/VehicleDash/MileageTrackerCard/index.js
@@ -18,17 +18,7 @@ function MyVerticallyCenteredModal(props) {
                     Modal heading
           </Modal.Title>
             </Modal.Header>
-            <Modal.Body>
-                <h4>Centered Modal</h4>
-                <p>
-                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                    consectetur ac, vestibulum at eros.
-          </p>
-            </Modal.Body>
-            <Modal.Footer>
-                <button onClick={props.onHide}>Close</button>
-            </Modal.Footer>
+          
         </Modal>
     );
 }
