efeae17975e09b3faaa5ed4bb09728dbbd0b6f8d
Gedalya Krycer <gedalya@krycer.com>
2020-09-09 22:25:37 -0700

    Fixed the content edit section

--- client/src/components/UserDash/UserDashPage/index.js
+++ client/src/components/UserDash/UserDashPage/index.js
@@ -4,114 +4,25 @@ import "./style.css";
 import { useDriverKickContext } from '../../../utils/DriverKickContext';
 import GlobalNavBar from '../../GlobalNavBar';
 import UserMainWrapper from '../UserMainWrapper';
-import UserVehicleCard from '../UserVehicleCard';
-import carIcons from "../../../utils/carIcons.json";
 import API from "../../../utils/API";
 
 
 
 function UserDashPage() {
 
-    const { setNavType, userData, logout, selectValue } = useDriverKickContext();
+    const { setNavType, logout } = useDriverKickContext();
 
     // Controls which buttons in the nav bar to use 
     setNavType("userDash");
 
-    // Stores vehicle info from the database
-    const [vehicleInfo, setVehicleInfo] = useState([]);
-
-    // Stores vehicle icon from the Switch 
-    const [iconImage, setIconImage] = useState({});
 
     //redirect to vehicle dashboard
     const redirect = useHistory();
 
-    
-    // START API Call to Database ————————————————|
-    useEffect(() => {
-        API.getUser()
-            .then((res) => {
-
-                // Validates if the user is signed in
-                if (res.data.isAuthenticated === false) {
-                    return logout(redirect);
-                }
-                const { vehicles } = res.data;
-                console.log(vehicles);
-                setVehicleInfo(vehicles);
-                
-                // const deepCopy = Object.assign([], res.data)
-                // setVehicleInfo(deepCopy);
-
-                // THIS WORKS
-                // setVehicleInfo(res.data[0]);
-
-
-            })
-            .catch((err) => console.log(err));
-    }, []);
-    // END API Call to Database ————————————————|
-
-
-    // START Switch for Car Icons ———————————————|
-    useEffect(() => {
-        switch (selectValue) {
-            case "convertible":
-                setIconImage(carIcons[0].image);
-                break;
-            case "miniVan":
-                setIconImage(carIcons[1].image);
-                break;
-            case "motorcycle":
-                setIconImage(carIcons[2].image);
-                break;
-            case "pickup":
-                setIconImage(carIcons[3].image);
-                break;
-            case "rv":
-                setIconImage(carIcons[4].image);
-                break;
-            case "sedan":
-                setIconImage(carIcons[5].image);
-                break;
-            case "sportsCar":
-                setIconImage(carIcons[6].image);
-                break;
-            case "suv":
-                setIconImage(carIcons[7].image);
-                break;
-            case "van":
-                setIconImage(carIcons[8].image);
-                break;
-            default:
-                setIconImage(carIcons[5].image);
-        }
-    }, []);
-    // END Switch for Car Icons ———————————————|
-
     return (
         <section>
             <GlobalNavBar />
-            <UserMainWrapper>
-                {/* {vehicleInfo.map(v => (
-                    <UserVehicleCard
-                        key={v._id}
-                        vehicleIcon={v.iconImage}
-                        vehicleMake={v.vehicleInfo.make}
-                        vehicleYear={v.vehicleInfo.year}
-                        vehicleModel={v.vehicleInfo.model}
-                    />
-                ))} */}
-
-                <UserVehicleCard
-                    vehicleIcon={iconImage}
-                    vehicleMake={vehicleInfo.make}
-                    vehicleYear={vehicleInfo.year}
-                    vehicleModel={vehicleInfo.model}
-                />
-
-
-            </UserMainWrapper>
+            <UserMainWrapper />
         </section>
     );
 }
--- client/src/components/UserDash/UserMainWrapper/index.js
+++ client/src/components/UserDash/UserMainWrapper/index.js
@@ -1,13 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import { useParams, useHistory } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import API from "../../../utils/API";
+import UserVehicleCard from '../UserVehicleCard';
+import carIcons from "../../../utils/carIcons.json";
 import { useDriverKickContext } from "../../../utils/DriverKickContext";
 
-function UserMainWrapper({ children }) {
-  const { userData, setUserData, logout } = useDriverKickContext();
+function UserMainWrapper() {
+  const { userData, setUserData, selectValue, logout } = useDriverKickContext();
+
+  // Stores vehicle info from the database
+  const [vehicleInfo, setVehicleInfo] = useState([]);
+
+  // Stores vehicle icon from the Switch 
+  const [iconImage, setIconImage] = useState({});
+
+
+
   const history = useHistory();
   const { id } = useParams();
 
@@ -21,7 +32,67 @@ function UserMainWrapper({ children }) {
       })
       .catch((err) => console.log(err));
     console.log(userData);
+
+    API.getVehicles()
+      .then((res) => {
+        if (res.data.isAuthenticated === false) {
+          return logout(history);
+        }
+        setVehicleInfo(res.data);
+      })
+      .catch((err) => console.log(err));
+
+
+
   }, []);
+
+
+  const getLatestVehicles = () => {
+    API.getVehicles()
+      .then((res) => {
+        if (res.data.isAuthenticated === false) {
+          return logout(history);
+        }
+        setVehicleInfo(res.data);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  // START Switch for Car Icons ———————————————|
+  useEffect(() => {
+    switch (selectValue) {
+      case "convertible":
+        setIconImage(carIcons[0].image);
+        break;
+      case "miniVan":
+        setIconImage(carIcons[1].image);
+        break;
+      case "motorcycle":
+        setIconImage(carIcons[2].image);
+        break;
+      case "pickup":
+        setIconImage(carIcons[3].image);
+        break;
+      case "rv":
+        setIconImage(carIcons[4].image);
+        break;
+      case "sedan":
+        setIconImage(carIcons[5].image);
+        break;
+      case "sportsCar":
+        setIconImage(carIcons[6].image);
+        break;
+      case "suv":
+        setIconImage(carIcons[7].image);
+        break;
+      case "van":
+        setIconImage(carIcons[8].image);
+        break;
+      default:
+        setIconImage(carIcons[5].image);
+    }
+  }, []);
+  // END Switch for Car Icons ———————————————|
 
   return (
     <section className="g__dashboard-wrapper">
@@ -35,7 +106,22 @@ function UserMainWrapper({ children }) {
             </p>
           </Col>
           <Col lg={1}></Col>
-          <Col lg={8}>{children}</Col>
+          <Col lg={8}>
+            {vehicleInfo.map(v => (
+              <UserVehicleCard
+                key={v._id}
+                vehicleID={v._id}
+                vehicleIcon={v.iconImage}
+                vehicleMake={v.make}
+                vehicleYear={v.year}
+                vehicleModel={v.model}
+                carNickname={v.nickname}
+                ownerName={v.driverName}
+                getLatestVehicles={getLatestVehicles}
+
+              />
+            ))}
+          </Col>
         </Row>
       </div>
     </section>
