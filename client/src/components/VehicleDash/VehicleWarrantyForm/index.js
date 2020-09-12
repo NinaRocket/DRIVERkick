b36f691a7af38cb8737afa2467077060c11e0396
Gedalya Krycer <gedalya@krycer.com>
2020-09-11 18:26:42 -0700

    Fixed warranty submit button from not closing the modal

--- client/src/components/VehicleDash/VehicleWarrantyForm/index.js
+++ client/src/components/VehicleDash/VehicleWarrantyForm/index.js
@@ -1,56 +1,16 @@
-import React, { useState } from "react";
-import API from "../../../utils/API";
+import React from "react";
 import "./style.css";
 import Form from 'react-bootstrap/form';
 
-function VehicleWarrantyForm() {
-  const [title, setWarrantyTitle] = useState([]);
-  const [provider, setWarrantyProvider] = useState([]);
-  const [details, setWarrantyDetails] = useState([]);
-
-  const addWarrantyTitle = (event) => {
-    setWarrantyTitle(event.target.value);
-  };
-
-  const addWarrantyProvider = (event) => {
-    setWarrantyProvider(event.target.value);
-  };
-
-  const addWarrantyDetails = (event) => {
-    setWarrantyDetails(event.target.value);
-  };
-
-  const submitNewWarranty = (event) => {
-    event.preventDefault();
-
-    const warrantyInfo = {
-      title: title,
-      provider: provider,
-      details: details,
-      //vehicle:
-    };
-
-    API.newWarranty(warrantyInfo)
-      .then((response) => {
-        console.log("new warranty response: ");
-        console.log(response);
-        if (!response.data.errmsg) {
-          console.log("successfully added warranty");
-          console.log(warrantyInfo);
-          //   this.state({
-          //     //redirect to login page
-          //     //home for now
-          //     redirectTo: "/home",
-          //   });
-        } else {
-          console.log("Nina doesn't know how to code");
-        }
-      })
-      .catch((error) => {
-        console.log("adding warranty error: ");
-        console.log(error);
-      });
-  };
+function VehicleWarrantyForm({
+  title,
+  provider,
+  details,
+  addWarrantyTitle,
+  addWarrantyProvider,
+  addWarrantyDetails,
+  submitWarrantyForm
+}) {
 
   return (
     <form className="g__deep-blue--txt">
@@ -62,7 +22,6 @@ function VehicleWarrantyForm() {
             id="warrantyTitle"
             name="warrantyTitle"
             placeholder="Name of warranty…"
-            value={title}
             onChange={addWarrantyTitle} />
         </Form.Group>
       </div>
@@ -77,7 +36,6 @@ function VehicleWarrantyForm() {
             id="warrantyProvider"
             name="warrantyProvider"
             placeholder="Store or company..."
-            value={provider}
             onChange={addWarrantyProvider} />
         </Form.Group>
       </div>
@@ -91,7 +49,6 @@ function VehicleWarrantyForm() {
             id="warrantyDetails"
             name="warrantyDetails"
             placeholder="Provide any relevant notes that will need to be referred to in the future…"
-            value={details}
             onChange={addWarrantyDetails} />
         </Form.Group>
       </div>
@@ -99,7 +56,7 @@ function VehicleWarrantyForm() {
 
       <button
         className="btn g__form-submit-btn"
-        onClick={submitNewWarranty}
+        onClick={submitWarrantyForm}
         type="submit"
       >Add Warranty</button>
     </form>
