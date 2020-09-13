67c315005171870fec9e946f17a96703410a236f
NRocket <ninamrocket@gmail.com>
2020-09-12 20:56:13 -0700

    mileage history works

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -39,15 +39,22 @@ module.exports = {
       .catch((err) => res.status(422).json(err));
   },
   updateMileage: (req, res) => {
-    db.Vehicle.findOneAndUpdate({ _id: req.params.id },
-      { 
+    db.Vehicle.findOneAndUpdate(
+      { _id: req.params.id },
+      {
         currentMileage: req.body.currentMileage,
         lastMileageUpdate: Date.now(),
-        mileageHistory: { $push: { date: Date.now(), mileage: req.body.currentMileage }}
+        $push: {
+          mileageHistory: {
+            date: Date.now(),
+            mileage: req.body.currentMileage,
+          },
+        },
       },
-      { new: true })
-      .then(dbVehicle => res.json(dbVehicle))
-      .catch(err => res.status(422).json(err));
+      { new: true }
+    )
+      .then((dbVehicle) => res.json(dbVehicle))
+      .catch((err) => res.status(422).json(err));
   },
   remove: function (req, res) {
     db.Vehicle.findOneAndDelete({ _id: req.params.id })
