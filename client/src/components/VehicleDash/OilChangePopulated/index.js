37f91ff3ad4acaa102ad1c2ab36e97cb6dc06783
Gedalya Krycer <gedalya@krycer.com>
2020-09-13 04:19:54 -0700

    Added progress bar state

--- client/src/components/VehicleDash/OilChangePopulated/index.js
+++ client/src/components/VehicleDash/OilChangePopulated/index.js
@@ -6,6 +6,7 @@ import Accordion from "react-bootstrap/Accordion";
 import API from "../../../utils/API";
 import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
 import oilCan from '../../../images/vehiclepage/oil-can.svg'
+import oilCanFire from '../../../images/vehiclepage/oil-can-fire.svg'
 import { useDriverKickContext } from "../../../utils/DriverKickContext";
 import ProgressBar from 'react-bootstrap/ProgressBar'
 
@@ -14,7 +15,7 @@ import ProgressBar from 'react-bootstrap/ProgressBar'
 // Warranty COMPONENT ==============================
 function OilChangePopulated({ oilChangeTrackingModal, warranty }) {
 
-
+  const [status, setStatus] = useState(70);
 
   return (
 
@@ -22,17 +23,17 @@ function OilChangePopulated({ oilChangeTrackingModal, warranty }) {
       <div className="oil-change-card__body">
         <div className="oil-change-card__number">
           <h4 className="g__sky-blue--txt">Miles to next Oil Change</h4>
-          <h3 className="oil-change-card__mileage-txt">3,023</h3>
+          <h3 className={`oil-change-card__mileage-txt ${status < 80 ? null : "g__fiery-orange--txt"}`}>3,023</h3>
 
         </div>
         <div className="oil-change-card__progress-container">
 
           <ProgressBar
-            now={60}
-            className="oil-change-card__progress-bar"
+            now={status}
+            className={`oil-change-card__progress-bar ${status < 80 ? null : "g__progress-override"}`}
           />
 
-          <img src={oilCan} alt="Oil Can" className="oil-change-card__progress-icon"/>
+          <img src={status < 80 ? oilCan : oilCanFire} alt="Oil Can" className="oil-change-card__progress-icon" />
         </div>
       </div>
 
